d8226d7d74a814b467d8e458a7875478a3c84ea2
zdy <287930548@qq.com>
2019-08-06 11:00:44 +0800

    收藏

--- lib/widget/article_widget.dart
+++ lib/widget/article_widget.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:wanandroid_flutter/entity/article_list_entity.dart';
-class ArticleItemWidget extends StatelessWidget {
 
+class ArticleItemWidget extends StatefulWidget {
   final ArticleData data;
   final int index;
 
   ArticleItemWidget(this.data, this.index);
 
   @override
+  State<StatefulWidget> createState() {
+    return ArticleItemState();
+  }
+}
+
+class ArticleItemState extends State<ArticleItemWidget> {
+  @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
         Navigator.of(context).push(MaterialPageRoute(builder: (context) {
           return WebviewScaffold(
-            url: data.link,
+            url: widget.data.link,
             appBar: AppBar(
-              title: Text(data.title),
+              title: Text(widget.data.title),
             ),
           );
         }));
@@ -30,11 +37,11 @@ class ArticleItemWidget extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Text(
-                    '${index + 1}  ${data.author}',
+                    '${widget.index + 1}  ${widget.data.author}',
                     style: TextStyle(fontSize: 12),
                   ),
                   Text(
-                    '${data.niceDate}',
+                    '${widget.data.niceDate}',
                     style: TextStyle(fontSize: 12),
                   )
                 ],
@@ -43,20 +50,33 @@ class ArticleItemWidget extends StatelessWidget {
                 margin: EdgeInsets.symmetric(vertical: 8),
                 alignment: Alignment.centerLeft,
                 child: Text(
-                  '${data.title}',
-                  style: TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold),
+                  '${widget.data.title}',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
                 ),
               ),
               Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  '${data.superChapterName}/${data.chapterName}',
-                  style: TextStyle(fontSize: 12),
-                ),
-              )
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        '${widget.data.superChapterName}/${widget.data.chapterName}',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                      GestureDetector(
+                        child: widget.data.collect
+                            ? Icon(Icons.star)
+                            : Icon(Icons.star_border),
+                        onTap: () {
+                          setState(() {
+                            widget.data.collect = !widget.data.collect;
+                          });
+                        },
+                      )
+                    ],
+                  ))
             ],
           )),
     );
